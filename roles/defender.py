83db6e063fdc18638bb9faeeaa14abf03ad5216b
LuMingJun62511 <90137882+LuMingJun62511@users.noreply.github.com>
2024-02-17 19:41:41 +0000

    try to implemet strategy point

--- roles/defender.py
+++ roles/defender.py
@@ -78,3 +78,58 @@ class Defender(Role):
         strategic_pos = strategic_position()
         direction_to_strategic_pos = get_direction({'x': self.x, 'y': self.y}, strategic_pos)
         return {'type': 'move', 'player_number': self.number, 'destination': strategic_pos, 'direction': direction_to_strategic_pos, 'speed': 7}
+
+
+    # 当球不在我方半场时，后卫应该根据他们相对于球门的位置来选择球门。如果两名后卫都在同一侧，那么他们需要根据距离另一个球门的远近来选择谁去防守哪个球门。
+    # 当球在我方半场，无论另一后卫是在追球还是持球，总之，当它不在正常防守，我就要补弱，否则，我们呢正常防守，且对手持球，最近的后卫追球，另一名后卫应选择远离球的球门来进行防守。这两个合并
+    # 当球在我方半场，我方持球但本人不持球时，应考虑补弱侧的策略，即选择相对远离球的球门。
+
+    def calculate_distances_in_strategy(defender_itself, defender_other, goalpost_1, goalpost_2,ball):
+        defender_itself_to_goalpost_1 = get_distance({'x': defender_itself['x'], 'y': defender_itself['y']}, {'x': goalpost_1['x'], 'y': goalpost_1['y']})
+        defender_itself_to_goalpost_2 = get_distance({'x': defender_itself['x'], 'y': defender_itself['y']}, {'x': goalpost_2['x'], 'y': goalpost_2['y']})
+        defender_other_to_goalpost_1 = get_distance({'x': defender_other['x'], 'y': defender_other['y']}, {'x': goalpost_1['x'], 'y': goalpost_1['y']})
+        defender_other_to_goalpost_2 = get_distance({'x': defender_other['x'], 'y': defender_other['y']}, {'x': goalpost_2['x'], 'y': goalpost_2['y']})
+        
+        defender_itself_to_ball = get_distance({'x': defender_other['x'], 'y': defender_other['y']}, {'x': ball['x'], 'y': ball['y']})
+        defender_other_to_ball = get_distance({'x': defender_other['x'], 'y': defender_other['y']}, {'x': ball['x'], 'y': ball['y']})
+
+        return defender_itself_to_goalpost_1,defender_itself_to_goalpost_2,defender_other_to_goalpost_1,defender_other_to_goalpost_2,defender_itself_to_ball,defender_other_to_ball
+
+
+    def determine_goalpost_by_sidedness(defender_itself, defender_other, goalpost_1, goalpost_2,ball):
+        defender_itself_to_goalpost_1,defender_itself_to_goalpost_2,defender_other_to_goalpost_1,defender_other_to_goalpost_2,defender_itself_to_ball,defender_other_to_ball = calculate_distances_in_strategy(defender_itself, defender_other, goalpost_1, goalpost_2,ball)
+        goalpost = goalpost_1
+        if not self.on_both_sides(ball):#如果两名后卫在异侧
+            goalpost = goalpost_1 if defender_itself_to_goalpost_1 < defender_itself_to_goalpost_2 else goalpost_2
+        else:   #如果两名后卫在同侧
+            if defender_itself_to_goalpost_1 < defender_itself_to_goalpost_2: #在同侧，且都离门柱1近
+                goalpost = goalpost_2 if defender_itself_to_goalpost_2 < defender_other_to_goalpost_2 else goalpost_1 #相对离门柱2近的过去
+            else: #在同侧，且都离门柱2近
+                goalpost = goalpost_1 if defender_itself_to_goalpost_1 < defender_other_to_goalpost_1 else goalpost_2 #相对离门柱2近的过去
+        return goalpost
+
+    def choose_goalpost(defender_itself, defender_other, goalpost_1, goalpost_2,ball):
+        goalpost = goalpost_1
+        defender_itself_to_goalpost_1,defender_itself_to_goalpost_2,defender_other_to_goalpost_1,defender_other_to_goalpost_2,defender_itself_to_ball,defender_other_to_ball = calculate_distances_in_strategy(defender_itself, defender_other, goalpost_1, goalpost_2,ball)
+
+        if not self.own_half(ball):# 当球不在我方半场时
+            goalpost = determine_goalpost_by_sidedness(defender_itself, defender_other, goalpost_1, goalpost_2,ball)
+        else:#当球在我方半场
+            if not self.red_team_owns_ball(ball):#我方还不持球，一旦另一个后卫在追球或持球，则我要补它另一侧，
+                if defender_itself_to_ball > defender_other_to_ball:#我不追球，就要看另一人
+                    goalpost = goalpost_2 if defender_other_to_goalpost_1 < defender_other_to_goalpost_2 else goalpost_1#如果追球人离1柱比较近，我就去守2柱
+            elif not self.red_team_owns_ball(ball) :#我方持球但我不持球，
+                if not self.owns_ball(defender_other,ball):#如果另一队员和我一样不持球，则考虑一般性的异侧正常，同侧补弱，
+                    goalpost = determine_goalpost_by_sidedness(defender_itself, defender_other, goalpost_1, goalpost_2,ball)
+                else: #如果另一人持球，则它离1柱比较近，我去守2柱
+                    goalpost = goalpost_2 if defender_other_to_goalpost_1 < defender_other_to_goalpost_2 else goalpost_1
+        return goalpost
+
+
+    def on_both_sides(defender_itself, defender_other):
+        # 都在中线或一个在中线就视为异侧
+        return defender_itself['y']*defender_other['y'] > 0
+
+
+    def red_team_owns_ball(ball):
+        return ball['owner_color'] == 'red'
